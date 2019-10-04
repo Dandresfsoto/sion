42e6f351611fba9c297ac77e23acbbf20b8dc94a
dandresfsoto@gmail.com <maquinas2008%>
2019-10-03 21:38:17 -0500

    Prueba 2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -27,8 +27,8 @@ AZURE_CONTAINER = env('AZURE_CONTAINER')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # noqa F405
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # noqa F405
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AZURE_CUSTOM_DOMAIN = env('AZURE_CUSTOM_DOMAIN')
 MEDIA_URL = env('MEDIA_URL')
