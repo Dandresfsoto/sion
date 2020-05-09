8a0dfcf0c95d289b19487be85fcf8002975e96dc
dandresfsoto@gmail.com <dandresfsoto@gmail.com>
2020-05-09 15:11:23 -0500

    Delete reportes

--- direccion_financiera/admin.py
+++ direccion_financiera/admin.py
@@ -13,8 +13,10 @@ def delete_reporte(modeladmin, request, queryset):
     for reporte in queryset:
         pagos = models.Pagos.objects.filter(reporte = reporte)
         descuentos = models.Descuentos.objects.filter(pago__id__in = pagos.values_list('id',flat=True)).delete()
+        amortizaciones = models.Amortizaciones.objects.filter(pago__id__in = pagos.values_list('id',flat=True)).delete()
         pagos.delete()
         reporte.delete()
+
 delete_reporte.short_description = "Borrar reporte"
 
 class ReportesAdmin(admin.ModelAdmin):
